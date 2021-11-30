f257be2d98f6f11ed83a6862200d04ce0c5bc5f0
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-11-30 07:35:22 +0700

    feat: 🎸 Add IDK

--- Bloxfruit/Server/app.js
+++ Bloxfruit/Server/app.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import serverRoute from './routes/server.routes.js';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
 const app = express();
 const port = 80;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.status(200).json({
@@ -10,7 +18,6 @@ app.get('/', (req, res) => {
   return;
 });
 app.use('/server', serverRoute);
-
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
